f3aee0c1122e211114a08da3cd06cb517ee936ee
Aaryan126 <53483782+Aaryan126@users.noreply.github.com>
2020-12-10 22:09:00 +0800

    Update ChapterFourQuestionThree.java

--- ChapterFourQuestionThree.java
+++ ChapterFourQuestionThree.java
@@ -1,32 +1,21 @@
 public class ChapterFourQuestionThree {
 	
 	public static void main (String[] args) {
+	int x = 1;
+	int y = 1;
+	int num = IBIO.inputInt("Enter a number: ");
+	int ans = x*x + y*y; 
 	
-	//int x = IBIO.inputInt("Enter a number less than 1000: "); 
-	int sqrf = 1;
-	int sqrs = 1;
-	//int i = 0;
-	//int j = 0;
-	int ans = 0;
-	
-	
-	mainLoop : do {
-		int num = IBIO.inputInt("Enter a number less than 1000: ");
-			switch ( num ) {
-				case 1:
-					for (int i =1 ; i>num; i++) {
-						sqrf = i*i;
-						}
-					System.out.println("Possible" + );
-					break mainLoop;
-				case 2:
-					for (int j =1; j>num; j++) {
-						sqrf = j*j;
-						}
-					System.out.println("wrong");
-					break;
-					}
-		} while (true);
+	while(ans <= num) {
+		if (ans == num) {
+			System.out.println("Possible");
+			System.exit(0);
+			}
+		x = x+1;
+		y = y+1;
+		ans = x*x + y*y; 
+		}
+	System.out.println("Impossible");
 	}
 }
 
